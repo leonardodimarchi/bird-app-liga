e3662285c1824d86169a4b78a4d06cca2922e197
leonardo <leonardodimarchi@gmail.com>
2021-04-10 18:15:12 -0300

    feat(app): added initial files

---
+++ app/src/app/pages/main/main.page.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+// Classe inicial do projeto
+@Component({
+  selector: 'bird-main',
+  templateUrl: './main.page.html',
+  styleUrls: ['./main.page.scss'],
+})
+export class MainPage {
+
+  constructor() { }
+
+}
