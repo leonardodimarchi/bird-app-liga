97dd7f35debc8c2fb7851543ccbebb8e2cd7a956
leonardo <leonardodimarchi@gmail.com>
2021-04-15 16:34:51 -0300

    feat(backend): added initial configurations - setup for API

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,21 @@
+/* #region  Imports */
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TypeOrmService } from './modules/typeorm/type-orm.service';
+/* #endregion */
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useClass: TypeOrmService
+    }),
+    
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
